ebb67380c297c84453ce79fb288b2a0177435859
Pol Rubio Borrego <prubio@edu.tecnocampus.cat>
2024-03-25 22:00:53 +0100

    baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      apiBase: '/portal-lliga'
-    }
+  app: {
+    baseURL: '/PortalLliga'
   }
 })
